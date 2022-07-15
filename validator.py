fae8558157a0899c7197f90fc3ef34993abfebe8
Isaque Alves <alvesisaque@Isaques-MacBook-Air.local>
2022-07-15 10:50:23 -0300

    adding validator

--- validator.py
+++ validator.py
@@ -1,4 +1,5 @@
 import util
+import planner
 from state import State
 
 
@@ -12,5 +13,54 @@ def validate(problem, solution):
     It should give you some indication of the correctness of your planner,
     mainly for debugging purposes.
     '''
-    return solution[0] in problem.goal
+    if solution is None:
+        return True
+    else:
+        current_state = State().union(problem.init)
+        for action in solution:
+            if (not applicable(current_state, action)):
+                return False
+            else:
+                old_state = current_state
+                current_state = successor(current_state, action)
+                next_state_check(old_state, current_state, action)
+        if (not is_goal(current_state, problem.goal)):
+            return False
+        else:
+            return True
+
+
+def applicable(state, action):
+    applicable = True
+    intersect = state.intersect(action.precond)
+    for cond in action.precond:
+        if cond not in intersect:
+            applicable = False
+            break
+    return applicable
+
+
+def next_state_check(oldstate, newstate, action):
+    common = oldstate.intersect(newstate)
+    inter = newstate.intersect(action.pos_effect)
+    for effect in action.pos_effect:
+        if effect not in inter:
+            return False
+        if effect in common:
+            return False
+
+    inter = newstate.intersect(action.neg_effect)
+    for effect in action.neg_effect:
+        if effect in inter:
+            return False
+        if effect in common:
+            return False
+
+
+def successor(state, action):
+    return State(action.pos_effect).union(State(state).difference(action.neg_effect))
+
+
+def is_goal(state, goal):
+    return State(state).intersect(goal) == State(goal)
 
